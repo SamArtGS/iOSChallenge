fb9ec647ab4cca18683414c965da91f268381564
Samuel Garrido <samuelarturo@icloud.com>
2022-05-29 14:56:34 -0500

    TODO: Possible SwiftUI implementation of AlbumListView

---
+++ TangoChallenge/TangoChallenge/Presentation/View/SwiftUI/AlbumsListView.swift
@@ -0,0 +1,22 @@
+//
+//  AlbumsListView.swift
+//  TangoChallenge
+//
+//  Created by Samuel Arturo Garrido Sánchez on 2022-05-29.
+//
+
+import SwiftUI
+
+//TODO: Same Presentation Layer in SwiftUI
+
+struct AlbumsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumsListView()
+    }
+}
